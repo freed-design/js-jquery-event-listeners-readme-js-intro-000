d9b29a564fcea7d8b72f2b26ff717629dddd681d
Daniel Dunbar <danielqdunbar@gmail.com>
2018-03-21 01:26:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,20 +1,24 @@
 //define functions here
 function getIt() {
-  $('p').on("click", function()
+  $('p').on("click", function(){
     alert("Hey!");
     console.log();("getIt ran!")
-  ) 
+  })
 }
 
 function frameIt() {
-  $('img').on("load", function()
+  $('img').on("load", function(){
     $("img").addClass("tasty");
     console.log("framIt Ran!!!")
-  )
+  })
 }
 
 function pressIt() {
-
+  $('form input').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("G was pressed!")
+    }
+  })
 }
 
 
@@ -25,6 +29,6 @@ $(document).ready(function(){
 // call functions here
 
 
-  $('form input').on('keydown', pressIt())
+
 
 });
