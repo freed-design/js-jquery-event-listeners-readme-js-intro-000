b298760df9d09cab74ecff14d32ce560d51b823c
Daniel Dunbar <danielqdunbar@gmail.com>
2018-03-21 01:10:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,14 +3,23 @@ function getIt() {
   alert("Hey!");
 
 function frameIt() {
-  
+  $('img').addClass('tasty')
 }
-  
+
+function pressIt() {
+
+}
+
+function pressIt() {
+
+}
+
 }
 
 $(document).ready(function(){
 
 // call functions here
   $('p').on("click", getIt())
+  $('img').on("load", frameIt())
 
 });
